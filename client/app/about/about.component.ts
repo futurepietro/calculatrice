748bb7dc3376b094bdd71891367b64940fe76bb8
primopietro <Pietro.hyde@gmail.com>
2018-03-29 18:15:11 -0400

    feature/ baseCalculatrice

--- client/app/about/about.component.ts
+++ client/app/about/about.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
 
+enum SelectedSide {
+  None = 1,
+  Left,
+  Right
+}
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html'
 })
+
 export class AboutComponent {
 
-  constructor() { }
+  numberOne: number;
+  numberTwo: number;
+  result: number;
+  selectedSide: SelectedSide;
+
+  constructor() {
+    this.numberOne = 0;
+    this.numberTwo = 0;
+    this.result = 0;
+    this.selectedSide = 1;
+  }
+
+  public range(min, max, step) {
+    step = step || 1;
+    var input = [];
+    for (var i = min; i <= max; i += step) input.push(i);
+    return input;
+  }
 
 }
