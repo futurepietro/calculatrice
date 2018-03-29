16254a0c1952261778b9c04f38eaea2d3a9605fb
primopietro <Pietro.hyde@gmail.com>
2018-03-29 17:39:14 -0400

    feature/ initial commit to develop

---
+++ client/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { JwtModule } from '@auth0/angular-jwt';
+
+import { RoutingModule } from './routing.module';
+import { SharedModule } from './shared/shared.module';
+import { CatService } from './services/cat.service';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { CatsComponent } from './cats/cats.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CatsComponent,
+    AboutComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    RoutingModule,
+    SharedModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        // whitelistedDomains: ['localhost:3000', 'localhost:4200']
+      }
+    })
+  ],
+  providers: [
+    CatService
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
